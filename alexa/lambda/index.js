16758afc9f94d3343f03d6b9e05455b51c8f657c
jmlowenthal <jamie.lowenthal@gmail.com>
2019-01-19 17:55:20 +0000

    Add FallbackIntent and ConfirmIntent handlers

--- alexa/lambda/index.js
+++ alexa/lambda/index.js
@@ -2,6 +2,9 @@ const Alexa = require('ask-sdk-core');
 
 // POST /api/newMessage { senderName: "Jamie", senderIdentifier: uint64"xy27", message: "Hello there" }
 
+FALLBACK_MESSAGE = 'Sorry, but I can\'t understand your request.'
+FALLBACK_REPROMPT = 'How can I help?'
+
 const SendMessageIntentHandler = {
     canHandle(handlerInput) {
         const request = handlerInput.requestEnvelope.request;
@@ -16,15 +19,49 @@ const SendMessageIntentHandler = {
     }
 };
 
+const ConfirmIntentHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type == 'LaunchRequest'
+            || (request.type == 'IntentRequest'
+                && request.intent.name == 'Confirm.Intent.603332935488');
+    },
+    handle(handlerInput) {
+        return handlerInput.responseBuilder
+            .speak('Broadcasting your message')
+            .getResponse();
+    }
+}
+
+const FallbackIntentHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type == 'IntentRequest'
+            && request.intent.name == 'AMAZON.FallbackIntent';
+    },
+    handle(handlerInput) {
+        return handlerInput.responseBuilder
+            .speak(FALLBACK_MESSAGE)
+            .reprompt(FALLBACK_REPROMPT)
+            .getResponse();
+    },
+};
+
 const ErrorHandler = {
     canHandle() {
         return true;
     },
-    handle(input, error) {
+    handle(handlerInput, error) {
         console.log(`Error handled: ${error.message}`);
-        return input.responseBuilder
-            .speak('Sorry, an error occurred')
-            .reprompt('Sorry, an error occurred')
+        var message = 'Sorry, an error has occurred.';
+        if (process.env.DEBUG) {
+            const request = handlerInput.requestEnvelope.request;
+            message = `Error for type=${request.type}`
+                + `intent=${request.intent.name}: ${error.message}`;
+        }
+        return handlerInput.responseBuilder
+            .speak(message)
+            .reprompt(message)
             .getResponse();
     }
 };
@@ -32,7 +69,9 @@ const ErrorHandler = {
 exports.handler =
     Alexa.SkillBuilders.custom()
         .addRequestHandlers(
-            SendMessageIntentHandler
+            SendMessageIntentHandler,
+            ConfirmIntentHandler,
+            FallbackIntentHandler
         )
         .addErrorHandlers(
             ErrorHandler
