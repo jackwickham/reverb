b345fd2743b224cf1616dfa0af3ccb2d7b3c9f1d
LSWdigital <>
2019-01-19 18:06:46 +0000

    Added in-memory messageDB

--- backend/src/app/controller.go
+++ backend/src/app/controller.go
@@ -4,10 +4,65 @@ import (
 	"fmt"
 )
 
-func Controller(wsUnsent 	chan UnsentMessage,
-		wsPR 		chan PushRegistration,
-		wsMessageLoad 	chan MessageLoadRequest,
-		wsSend 		chan Message,
-		pnPush 		chan PushMessage) {
-	return
+func Controller(wsUnsent      chan UnsentMessage,
+		wsPR          chan PushRegistration,
+		wsMessageLoad chan MessageLoadRequest,
+		wsSend        chan Message,
+		pnPush        chan PushMessage) {
+
+	// Set up some channels
+	msgDB       := make(chan Message)
+	pushDB      := make(chan PushMessage)
+	msgDbQuery  := make(chan MessageLoadRequest)
+	pushDbQuery := make(chan UnsentMessage)
+	msgDbStore  := make(chan UnsentMessage)
+	pushDbStore := make(chan PushRegistration)
+
+	// Set up the databases
+	go mDB(wsSend, msgDbQuery, msgDbStore)
+	go pDB(pnPush, msgDbStore, pushDbStore)
+
+	// Deal with all the data
+	for{
+		select{
+			case message := <-wsUnsent:
+				msgDbStore <- message
+				pushDbQuery <- message
+			case pushReg := <-wsPR:
+				pushDbStore <- pushReg
+			case loadReq := <-wsMessageLoad:
+				msgDbQuery <- loadReq
+		}
+	}
+}
+
+func mDB(out   chan Message,
+	 query chan MessageLoadRequest,
+	 in    chan UnsentMessage){
+	messages := make([]Message, 0)
+
+	for{
+		select{
+			case message := <-in:
+				messages = append(messages, message.message)
+			case req := <-query:
+				x := req.lastSeenId + 1
+				for _, m := range messages[x:] {
+					out <- m
+				}
+		}
+	}
+}
+
+func pDB(out   chan PushMessage,
+	 query chan UnsentMessage,
+	 in    chan PushRegistration){
+	for{
+		select{
+			case pushReg := <-in:
+				//Store the registration
+			case message := <-query:
+				// send the push requests
+		}
+	}
 }
