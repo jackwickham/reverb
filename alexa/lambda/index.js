53971415ef8092c878999e98f16047a28045a58f
jmlowenthal <jamie.lowenthal@gmail.com>
2019-01-19 17:15:28 +0000

    Make it work

--- alexa/lambda/index.js
+++ alexa/lambda/index.js
@@ -1,13 +1,35 @@
 const Alexa = require('ask-sdk-core');
-const Handler = require('./handler');
-const Error = require('./error');
+
+const ReverbHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(input) {
+        return input.responseBuilder
+            .speak('To infinity and beyond')
+            .getResponse();
+    }
+};
+
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(input, error) {
+        console.log(`Error handled: ${error.message}`);
+        return input.responseBuilder
+            .speak('Sorry, an error occurred')
+            .reprompt('Sorry, an error occurred')
+            .getResponse();
+    }
+};
 
 exports.handler =
-    Alexa.SkillBuilders.standard()
+    Alexa.SkillBuilders.custom()
         .addRequestHandlers(
-            Handler.ReverbHandler
+            ReverbHandler
         )
         .addErrorHandlers(
-            Error.ErrorHandler
+            ErrorHandler
         )
         .lambda();
