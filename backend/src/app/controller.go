ffa4be321c19739607062c9bb5c2a56d3fe143af
LSWdigital <>
2019-01-19 16:31:23 +0000

    Added controller and started it

---
+++ backend/src/app/controller.go
@@ -0,0 +1,13 @@
+package app
+
+import (
+	"fmt"
+)
+
+func Controller(wsUnsent 	chan UnsentMessage,
+		wsPR 		chan PushRegistration,
+		wsMessageLoad 	chan MessageLoadRequest,
+		wsSend 		chan Message,
+		pnPush 		chan PushMessage) {
+	return
+}
--- backend/src/main.go
+++ backend/src/main.go
@@ -8,8 +8,12 @@ func main() {
 	newMessageChannel := make(chan app.UnsentMessage)
 	pushRegistrationChannel := make(chan app.PushRegistration)
 	getMessagesChannel := make(chan app.MessageLoadRequest)
+	sendMessagesChannel := make(chan app.Message)
+	pushMessagesChannel := make(chan app.PushMessage)
 
 	go app.Api(newMessageChannel, pushRegistrationChannel, getMessagesChannel)
+
+	go app.Controller(newMessageChannel, pushRegistrationChannel, getMessagesChannel, sendMessagesChannel, pushMessagesChannel)
 
 	// Wait forever
 	select { }
