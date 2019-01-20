27d16363045499c7a44edc84b112069f8ff004e8
Jack Wickham <jackwickham@live.co.uk>
2019-01-20 09:39:31 +0000

    Merge notifications

--- front/service-worker.js
+++ front/service-worker.js
@@ -27,11 +27,53 @@ self.addEventListener('fetch', (event) => {
     );*/
 });
 
+const notifyTag = "reverb-notify";
+
 self.addEventListener('push', function(event) {
+    let existingNotifications = self.registration.getNotifications({tag: notifyTag});
+    let pendingMessages = [];
+    if (existingNotifications.length > 0) {
+        pendingMessages = existingNotifications[0].data;
+    }
     let data = event.data.json();
-    let title = "New message from " + data.senderName;
-    self.waitUntil(self.registration.showNotification(title, {
-        body: data.body
+    pendingMessages.push(data);
+
+    console.log(pendingMessages);
+
+    let users = pendingMessages.map(msg => msg.senderName).filter((v, i, arr) => arr.indexOf(v) === i);
+    let userString;
+    if (users.length === 1) {
+        userString = users[0];
+    } else if (users.length <= 3) {
+        for (let i = 0; i < users.length - 1; i++) {
+            userString += userString[i] + ", ";
+        }
+        userString += "and " + userString[users.length - 1];
+    } else {
+        for (let i = 0; i < 2; i++) {
+            userString += userString[i] + ", ";
+        }
+        userString += `and ${users.length - 2} others`;
+    }
+
+    let title, body;
+    if (pendingMessages.length === 1) {
+        title = "New message from " + userString;
+        body = data.body;
+    } else if (pendingMessages.length <= 3) {
+        title = `${pendingMessages.length} new messages from ${userString}`;
+        body = pendingMessages.map(msg => msg.senderName + ": " + msg.body).join("\n");
+    } else {
+        title = `${pendingMessages.length} new messages from ${userString}`;
+        body = pendingMessages.slice(0, 2).map(msg => msg.senderName + ": " + msg.body).join("\n") + `\nand ${pendingMessages.length - 2} more...`;
+    }
+
+    console.log(title, body);
+
+    event.waitUntil(self.registration.showNotification(title, {
+        body: body,
+        data: pendingMessages,
+        tag: notifyTag
     }));
 });
   
