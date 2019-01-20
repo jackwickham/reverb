02c8452ed385354b140f5acce02c6f1fa04f1f91
Jack Wickham <jackwickham@live.co.uk>
2019-01-20 10:01:38 +0000

    Fix notification merging

--- front/service-worker.js
+++ front/service-worker.js
@@ -29,51 +29,55 @@ self.addEventListener('fetch', (event) => {
 
 const notifyTag = "reverb-notify";
 
-self.addEventListener('push', function(event) {
-    let existingNotifications = self.registration.getNotifications({tag: notifyTag});
-    let pendingMessages = [];
-    if (existingNotifications.length > 0) {
-        pendingMessages = existingNotifications[0].data;
-    }
-    let data = event.data.json();
-    pendingMessages.push(data);
-
-    console.log(pendingMessages);
-
-    let users = pendingMessages.map(msg => msg.senderName).filter((v, i, arr) => arr.indexOf(v) === i);
-    let userString;
-    if (users.length === 1) {
-        userString = users[0];
-    } else if (users.length <= 3) {
-        for (let i = 0; i < users.length - 1; i++) {
-            userString += userString[i] + ", ";
+self.addEventListener('push', async function(event) {
+    console.log("running");
+    event.waitUntil((async () => {
+        let existingNotifications = await self.registration.getNotifications({tag: notifyTag});
+        let pendingMessages = [];
+        if (existingNotifications.length > 0) {
+            pendingMessages = existingNotifications[0].data;
         }
-        userString += "and " + userString[users.length - 1];
-    } else {
-        for (let i = 0; i < 2; i++) {
-            userString += userString[i] + ", ";
+        console.log(pendingMessages);
+        let data = event.data.json();
+        pendingMessages.push(data);
+
+        console.log(pendingMessages);
+
+        let users = pendingMessages.map(msg => msg.senderName).filter((v, i, arr) => arr.indexOf(v) === i);
+        let userString;
+        if (users.length === 1) {
+            userString = users[0];
+        } else if (users.length <= 3) {
+            for (let i = 0; i < users.length - 1; i++) {
+                userString += userString[i] + ", ";
+            }
+            userString += "and " + userString[users.length - 1];
+        } else {
+            for (let i = 0; i < 2; i++) {
+                userString += userString[i] + ", ";
+            }
+            userString += `and ${users.length - 2} others`;
         }
-        userString += `and ${users.length - 2} others`;
-    }
 
-    let title, body;
-    if (pendingMessages.length === 1) {
-        title = "New message from " + userString;
-        body = data.body;
-    } else if (pendingMessages.length <= 3) {
-        title = `${pendingMessages.length} new messages from ${userString}`;
-        body = pendingMessages.map(msg => msg.senderName + ": " + msg.body).join("\n");
-    } else {
-        title = `${pendingMessages.length} new messages from ${userString}`;
-        body = pendingMessages.slice(0, 2).map(msg => msg.senderName + ": " + msg.body).join("\n") + `\nand ${pendingMessages.length - 2} more...`;
-    }
+        let title, body;
+        if (pendingMessages.length === 1) {
+            title = "New message from " + userString;
+            body = data.body;
+        } else if (pendingMessages.length <= 3) {
+            title = `${pendingMessages.length} new messages from ${userString}`;
+            body = pendingMessages.map(msg => msg.senderName + ": " + msg.body).join("\n");
+        } else {
+            title = `${pendingMessages.length} new messages from ${userString}`;
+            body = pendingMessages.slice(0, 2).map(msg => msg.senderName + ": " + msg.body).join("\n") + `\nand ${pendingMessages.length - 2} more...`;
+        }
 
-    console.log(title, body);
+        console.log(title, body);
 
-    event.waitUntil(self.registration.showNotification(title, {
-        body: body,
-        data: pendingMessages,
-        tag: notifyTag
-    }));
+        await self.registration.showNotification(title, {
+            body: body,
+            data: pendingMessages,
+            tag: notifyTag
+        });
+    })());
 });
   
